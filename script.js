9a5f272198af2bb88a4bb65c4bdaf7b7f69343f2
Romain DELIMAL <rom.delimal@gmail.com>
2023-01-04 19:17:38 +0100

    ProjectsDropdownDone

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const body = document.querySelector('body');
   navCloseBtn = document.querySelector('.navCloseBtn');
   logo = document.querySelector('.logo');
   navLinks = document.querySelector('.nav-links');
+  projectsMenu = document.querySelector('.projects-menu');
+  projectsDropdownLinks = document.querySelector('.projects-dropdown-links');
 
 // ===== Toggle Dark/Light Mode ===== //
 
@@ -18,37 +20,52 @@ modeToggle.addEventListener('click', () => {
   modeIcon.classList.replace("uil-sun", "uil-moon");
 });
 
+// ===== Toggle Search Bar ===== //
+
 searchIcon.addEventListener('click', () => {
   nav.classList.toggle('openSearch');
   nav.classList.remove('openNav');
+  if (projectsDropdownLinks.classList.contains('openProject')) {
+    projectsDropdownLinks.classList.remove('openProject');
+  }
   if(nav.classList.contains('openSearch')) {
     return searchIcon.classList.replace("uil-search", "uil-times");
   }
   searchIcon.classList.replace("uil-times", "uil-search");
-
 });
+
+// ===== Toggle Sidebar ===== //
 
 navOpenBtn.addEventListener('click', () => {
   nav.classList.add('openNav');
   nav.classList.remove('openSearch');
   searchIcon.classList.replace("uil-times", "uil-search");
-
 });
 navCloseBtn.addEventListener('click', () => {
   nav.classList.remove('openNav');
 });
 
+// ===== Toggle with Logo / Nav Links ===== //
+
 logo.addEventListener('click', () => {
   if (nav.classList.contains('openSearch')) {
     nav.classList.remove('openSearch');
     searchIcon.classList.replace("uil-times", "uil-search");
-
+  }
+  if (projectsDropdownLinks.classList.contains('openProject')) {
+    projectsDropdownLinks.classList.remove('openProject');
   }
 });
+
 navLinks.addEventListener('click', () => {
   if (nav.classList.contains('openSearch')) {
     nav.classList.remove('openSearch');
     searchIcon.classList.replace("uil-times", "uil-search");
-
   }
+});
+
+// ===== Toggle Projects Dropdown ===== //
+
+projectsMenu.addEventListener('click', () => {
+  projectsDropdownLinks.classList.toggle('openProject');
 });
